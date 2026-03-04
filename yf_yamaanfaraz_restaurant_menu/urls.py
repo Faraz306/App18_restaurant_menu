d56f8f96ae402ccaf94592e922aa87f2b026663d
Faraz <farazmoin.uk@gmail.com>
2026-03-04 06:59:50 -0500

    Added links.

--- yf_yamaanfaraz_restaurant_menu/urls.py
+++ yf_yamaanfaraz_restaurant_menu/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
     path("", views.MenuList.as_view(), name="home"),
+    path("item/<int:pk>/", views.MenuItemDetail.as_view(), name="menu_item"),
 ]
